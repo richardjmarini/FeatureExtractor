59a59d4c194c472c7dc82ea8f65ef397d2f6f31a
Ubuntu <ubuntu@ip-10-101-133-181.ec2.internal>
2014-05-01 20:17:59 +0000

    working on recursively walking decission tree

--- src/parse.py
+++ src/parse.py
@@ -1,22 +1,64 @@
 #!/usr/bin/env python
 
+from itertools import izip, chain
 from json import loads
 from os import path, pardir, curdir
 from sys import stdin, stderr, stdout, argv
 from optparse import OptionParser, make_option
+from nltk import word_tokenize, pos_tag
 
-class Parser:
+from address import AddressClassifier
+from featurebase import Token
+
+class Parser(AddressClassifier):
+
+   default_classification= ["OTHER"]
 
    def __init__(self, input= stdin, output= stdout):
 
       self.input= input
       self.output= output
 
+   def classify(self, token, feature_dict, feature_tree):
+
+      for feature in feature_tree:
+         value= feature_dict[feature]
+         subtree= feature_tree[feature]
+         cls= subtree[value]
+
+         print "\t%s|||||%s|||||%s" % (value, subtree, subtree)
+         if type(cls) == dict:
+            cls= self.classify(token, feature_dict, cls)
+         else:
+            return cls
+      return cls
+
+
+
    def parse(self, feature_tree):
 
-      for document in self.input:
-          p
       print feature_tree
+
+      for document in self.input:
+
+         tokens= [Token(*word) for word in pos_tag(word_tokenize(''.join(document)))]
+         map(self.add_features, tokens)
+
+         feature_list= list(chain(*[token.features for token in tokens]))
+         for token in tokens:
+
+            feature_dict= dict([(feature, False) for feature in feature_list])
+            feature_dict.update([(feature, True) for feature in token.features])
+            feature_dict.update([('POS', token.pos), ('CLASS', token.classification)])
+
+            print "-------------------------------------------------------------------"
+            print token.word
+            cls=  self.classify(token, feature_dict, feature_tree)
+
+
+         #print '>>>>', [(token.word, token.pos, token.classification, token.features) for token in tokens]
+         #tokens= [Token(*word) for word in map(lambda word: list(word) + self.default_classification, pos_tag(word_tokenize(''.join(document))))]
+
 
 
 def parse_args(argv):
@@ -33,7 +75,7 @@ def parse_args(argv):
 
    opts, args= optParser.parse_args()
    if opts.input == stdin:
-      setattr(opts, "input", stdin.read())
+      setattr(opts, "input", [stdin.read()])
    else:
       filenames= glob(opts.input) if '*' in opts.input else [opts.input]
       setattr(opts, 'input', [open(filename, 'r').read() for filename in filenames])
